d4c4bf8a2ae09663f70ac358d8c52c6f75bc2a74
Rebekah Park <rebekahpark@Rebekahs-MacBook-Pro.local>
2018-04-20 15:13:15 +0700

    Creating the Models -ing

--- djangoproject/images/models.py
+++ djangoproject/images/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from djangoproject.users import models as user_models #from to djangoproject/models/UserClass
 
+class TimeStampModel(models.Model): # Like a bluePrint Model For other Models
+    
+    created_at = models.DateTimeField(auto_now_add=True) #auto_now_add : its automatically, whenever i want add something.
+    updated_at = models.DateTimeField(auto_now=True) # self refresh
+
+    class Meta(): # MetaClass: Anything that's not field.
+        abstract = True # Now this Model, become a abstract base class.(this not related to database)
+        # Like a AbstractUser. Helping the others model
 # Create your models here.
+
+    """Image Model"""
+
+class Image(TimeStampModel): 
+    
+    file = models.ImageField()
+    location = models.CharField(max_length=140)
+    caption = models.TextField() # caption: 사진,잡화등에 붙인 정보
+    creator = models.ForeignKey(user_models.User, null = True, on_delete=models.PROTECT)
+
+    """Comment Model"""
+
+class Comment(TimeStampModel):
+
+    message = models.TextField()
+    creator = models.name = models.ForeignKey(user_models.User, null=True, on_delete=models.PROTECT)
+    image = models.ForeignKey(Image, null=True, on_delete=models.PROTECT)
+
+    """Like Model"""
+
+class Like(TimeStampModel):
+
+    creator = models.name = models.ForeignKey(user_models.User, null=True, on_delete=models.PROTECT)
+    image = models.name = models.ForeignKey(Image, null=True, on_delete=models.PROTECT)
--- djangoproject/users/models.py
+++ djangoproject/users/models.py
@@ -1,14 +1,28 @@
 from django.contrib.auth.models import AbstractUser
-from django.db import models
+from django.db import models 
 from django.urls import reverse
+from django.utils.encoding import python_2_unicode_compatible
 from django.utils.translation import ugettext_lazy as _
 
+GENDER_CHOICES = (
+    ('male','Male'),
+    ('female', 'Female'),
+    ('not specified', 'Not Specified')
+) 
+"""User Model"""
 
-class User(AbstractUser):
-
+@python_2_unicode_compatible  
+class User(AbstractUser): #Extend from models
     # First Name and Last Name do not cover name patterns
     # around the globe.
     name = models.CharField(_("Name of User"), blank=True, max_length=255)
+    website = models.URLField(null=True)
+    bio = models.TextField(null=True)
+    phone = models.CharField(max_length=140, null=True)
+    gender = models.CharField(max_length=80, choices=GENDER_CHOICES, null=True)
+    followers = models.ManyToManyField("self")
+    following = models.ManyToManyField("self")
+
 
     def __str__(self):
         return self.username
