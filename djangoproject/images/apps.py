3e45719f29692d3fd975c4374ddd7e72fdd62acc
Rebekah Park <rebekahpark@Rebekahs-MacBook-Pro.local>
2018-04-21 12:40:46 +0700

    Check the ManyToMany Field

--- djangoproject/images/apps.py
+++ djangoproject/images/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 
 class ImagesConfig(AppConfig):
-    name = 'djangoproject.images'
+    name = 'djangoproject.images' #if u make app, u have to register base.py "local_Apps"
