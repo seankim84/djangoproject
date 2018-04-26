27cfaba73ba70221a8fa6ad3df7354bcde2445b4
Rebekah Park <rebekahpark@Rebekahs-MacBook-Pro.local>
2018-04-26 12:54:20 +0700

    Refactoring Like / Unlike

--- djangoproject/images/views.py
+++ djangoproject/images/views.py
@@ -45,9 +45,7 @@ class LikeImage(APIView):
                 creator=user,
                 image=found_image
             )
-            preexisiting_like.delete()
-
-            return Response(status=status.HTTP_204_NO_CONTENT)
+            return Response(status=status.HTTP_304_NOT_MODIFIED)
 
         except models.Like.DoesNotExist:
 
@@ -59,6 +57,31 @@ class LikeImage(APIView):
             new_like.save()
 
             return Response(status=status.HTTP_201_CREATED)
+
+class UnLikeImage(APIView):
+
+    def delete(self, request, image_id, format=None):
+        
+        user = request.user
+
+        try:
+            found_image = models.Image.objects.get(id=image+id)
+
+        except models.Image.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        try: 
+            preexisiting_like = models.Like.objects.get(
+                creator=user,
+                image=found_image
+            )
+            preexisiting_like.delete()
+
+            return Response(status=status.HTTP_204_NO_CONTENT)
+
+        except models.Like.DoesNotExist:
+
+            return Response(status=status.HTTP_304_NOT_MODIFIED)
 
 class CommentOnImage(APIView):
 
