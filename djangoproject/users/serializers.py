f5ed6e269b981f3a28a7766a8d56e7ce4881dcdf
seankim84 <sean.kim84@gmail.com>
2018-05-04 13:09:03 +0700

    Uploading Profile Image

--- djangoproject/users/serializers.py
+++ djangoproject/users/serializers.py
@@ -5,7 +5,7 @@ from . import models
 
 class UserProfileSerializer(serializers.ModelSerializer):
 
-    images = images_serializers.CountImageSerializer(many=True)
+    images = images_serializers.CountImageSerializer(many=True, read_only=True)
     post_count = serializers.ReadOnlyField() #ReadOnlyField : 해당 필드는 수정하지 않는다!
     followers_count = serializers.ReadOnlyField()
     following_count = serializers.ReadOnlyField()
