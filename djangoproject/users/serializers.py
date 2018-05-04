6cc0a39a310163a97bdb77f5b8f319bb1ad71135
seankim84 <sean.kim84@gmail.com>
2018-05-04 11:13:20 +0700

    Update the Profile

--- djangoproject/users/serializers.py
+++ djangoproject/users/serializers.py
@@ -6,10 +6,14 @@ from . import models
 class UserProfileSerializer(serializers.ModelSerializer):
 
     images = images_serializers.CountImageSerializer(many=True)
+    post_count = serializers.ReadOnlyField() #ReadOnlyField : 해당 필드는 수정하지 않는다!
+    followers_count = serializers.ReadOnlyField()
+    following_count = serializers.ReadOnlyField()
 
     class Meta:
         model = models.User
         fields = (
+            'profile_image',
             'username',
             'name',
             'bio',
