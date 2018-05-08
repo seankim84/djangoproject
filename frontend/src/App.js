0fd11a87fef4c5c25770cf3279bddf1cfde84372
seankim84 <sean.kim84@gmail.com>
2018-05-09 01:13:19 +0700

    Fixed README

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import styles from './App.scss';
 
 
@@ -8,8 +7,7 @@ class App extends Component {
     return (
       <div className={styles.App}>
         <header className={styles.App__header}>
-          <img src={logo} className={styles.App__logo} alt="logo" />
-          <h1 className={styles.App_title}>Welcome to React</h1>
+          <h1 className={styles.App__title}>Welcome to React</h1>
         </header>
         <p className={styles.App__intro}>
           To get started, edit <code>src/App.js</code> and save to reload.
---
+++ frontend/src/redux/modules/users.js
@@ -0,0 +1,18 @@
+// imports
+
+// actions
+
+// action creators
+
+// initial state
+const initialState = {
+    isLoggedIn: localStorage
+}
+
+// reducer
+
+// reducer function
+
+// exports
+
+// reducer export
