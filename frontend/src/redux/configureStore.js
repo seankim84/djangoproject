877eddba9c856937625aba3bbe8418bfea2fb65b
seankim84 <sean.kim84@gmail.com>
2018-05-09 12:47:21 +0700

    Redux MiddleWares Thunk

--- frontend/src/redux/configureStore.js
+++ frontend/src/redux/configureStore.js
@@ -1,11 +1,14 @@
 //redux Store를 설정/구성한다.(Reducer 합치기)
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import users from 'redux/modules/users';
+
+const middlewares = [thunk];
 
 const reducer = combineReducers({ //combineReducer로 reducer들을 합친다.
     users
 })
 
-let store = initialState => createStore(reducer);
+let store = initialState => createStore(reducer, applyMiddleware(...middlewares)); // For make list of function, use "..." it means, unpack the array
 
-export default store();
+export default store(); 
